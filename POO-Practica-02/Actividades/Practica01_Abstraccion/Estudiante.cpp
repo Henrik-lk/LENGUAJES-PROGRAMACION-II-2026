1d49d1aedeca04ade96663abe7c68fd9c39617b1
Henrik-lk <micky.llave@estudiante.ucsm.edu.pe>
2026-03-21 22:08:53 -0500

    Implement Estudiante class with exam methods

--- POO-Practica-02/Actividades/Practica01_Abstraccion/Estudiante.cpp
+++ POO-Practica-02/Actividades/Practica01_Abstraccion/Estudiante.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
 
+class Estudiante {
+private:
+    string nombre;
+    int edad;
+    string grado;
+    float nota;
+
+public:
+    Estudiante(string n, int e, string g) {
+        nombre = n;
+        edad = e;
+        grado = g;
+        nota = 0;
+    }
+
+    void tomarExamen() {
+        cout << nombre << " está tomando el examen." << endl;
+    }
+
+    void recibirCalificacion(float n) {
+        nota = n;
+        cout << nombre << " recibió la nota " << nota << endl;
+    }
+};
+
+int main() {
+    Estudiante est1("Ana", 19, "3er ciclo");
+    Estudiante est2("Luis", 20, "4to ciclo");
+
+    est1.tomarExamen();
+    est1.recibirCalificacion(15);
+
+    est2.tomarExamen();
+    est2.recibirCalificacion(18);
+
+    return 0;
+}
