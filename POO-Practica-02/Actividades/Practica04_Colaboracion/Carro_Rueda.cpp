3df7d017cd4727e083e32c7576c7e45a00ab9a98
Henrik-lk <micky.llave@estudiante.ucsm.edu.pe>
2026-03-21 23:45:27 -0500

    Add Carro and Rueda classes with movement functionality

--- POO-Practica-02/Actividades/Practica04_Colaboracion/Carro_Rueda.cpp
+++ POO-Practica-02/Actividades/Practica04_Colaboracion/Carro_Rueda.cpp
@@ -1,1 +1,47 @@
+#include <iostream>
+using namespace std;
 
+class Rueda {
+private:
+    int tamano;
+
+public:
+    Rueda(int t) {
+        tamano = t;
+    }
+
+    int getTamano() {
+        return tamano;
+    }
+};
+
+class Carro {
+private:
+    Rueda r1, r2, r3, r4;
+
+public:
+    Carro(int t) : r1(t), r2(t), r3(t), r4(t) {}
+
+    void mover() {
+        cout << "El carro se mueve con ruedas de tamaño "
+             << r1.getTamano() << endl;
+    }
+
+    void cambiarRuedas(int nuevoTamano) {
+        r1 = Rueda(nuevoTamano);
+        r2 = Rueda(nuevoTamano);
+        r3 = Rueda(nuevoTamano);
+        r4 = Rueda(nuevoTamano);
+        cout << "Ruedas cambiadas\n";
+    }
+};
+
+int main() {
+    Carro c(16);
+
+    c.mover();
+    c.cambiarRuedas(18);
+    c.mover();
+
+    return 0;
+}
