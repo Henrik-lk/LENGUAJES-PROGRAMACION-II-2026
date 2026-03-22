b012aac5f40b9be8cb0cb455f05e21737839e4b7
Henrik-lk <micky.llave@estudiante.ucsm.edu.pe>
2026-03-21 22:42:34 -0500

    Add CuentaBancaria class with deposit/withdrawal methods
    Implemented a basic bank account class with deposit and withdrawal methods.

--- POO-Practica-02/Actividades/Practica02_Encapsulamiento/CuentaBancaria.cpp
+++ POO-Practica-02/Actividades/Practica02_Encapsulamiento/CuentaBancaria.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
+using namespace std;
 
+class CuentaBancaria {
+private:
+    double saldo;
+
+public:
+    string titular;
+
+    CuentaBancaria() {
+        saldo = 0;
+    }
+
+    void depositar(double monto) {
+        if (monto > 0)
+            saldo += monto;
+    }
+
+    void retirar(double monto) {
+        if (monto <= saldo)
+            saldo -= monto;
+        else
+            cout << "Saldo insuficiente" << endl;
+    }
+
+    double getSaldo() {
+        return saldo;
+    }
+};
+
+int main() {
+    CuentaBancaria c1;
+    c1.titular = "Ana";
+
+    c1.depositar(100);
+    c1.retirar(50);
+    c1.retirar(100); // inválido
+
+    cout << "Saldo: " << c1.getSaldo() << endl;
+}
