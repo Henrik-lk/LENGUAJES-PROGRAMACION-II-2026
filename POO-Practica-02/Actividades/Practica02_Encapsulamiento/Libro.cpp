ba0943de9a447c7c4b8d0289bf4a3add4de938ae
Henrik-lk <micky.llave@estudiante.ucsm.edu.pe>
2026-03-21 22:43:18 -0500

    Implement Libro class with encapsulation and methods

--- POO-Practica-02/Actividades/Practica02_Encapsulamiento/Libro.cpp
+++ POO-Practica-02/Actividades/Practica02_Encapsulamiento/Libro.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+using namespace std;
 
+class Libro {
+private:
+    string titulo;
+    string autor;
+    string genero;
+
+public:
+    void setTitulo(string t) { titulo = t; }
+    void setAutor(string a) { autor = a; }
+    void setGenero(string g) { genero = g; }
+
+    string getTitulo() { return titulo; }
+    string getAutor() { return autor; }
+    string getGenero() { return genero; }
+
+    void mostrarInfoLibro() {
+        cout << "Titulo: " << titulo << endl;
+        cout << "Autor: " << autor << endl;
+        cout << "Genero: " << genero << endl;
+    }
+};
+
+int main() {
+    Libro l1, l2;
+
+    l1.setTitulo("C++ Basico");
+    l1.setAutor("Juan");
+    l1.setGenero("Educativo");
+
+    l2.setTitulo("POO");
+    l2.setAutor("Maria");
+    l2.setGenero("Tecnico");
+
+    l1.mostrarInfoLibro();
+    cout << endl;
+    l2.mostrarInfoLibro();
+}
