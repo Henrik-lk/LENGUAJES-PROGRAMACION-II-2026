4cc3496f7b9e999f9a022e8917584d84e005c167
Henrik-lk <micky.llave@estudiante.ucsm.edu.pe>
2026-03-21 23:44:59 -0500

    Implement Libro and Biblioteca classes

--- POO-Practica-02/Actividades/Practica04_Colaboracion/Biblioteca_Libro.cpp
+++ POO-Practica-02/Actividades/Practica04_Colaboracion/Biblioteca_Libro.cpp
@@ -1,1 +1,52 @@
+#include <iostream>
+using namespace std;
 
+class Libro {
+private:
+    string titulo;
+    string autor;
+    bool prestado;
+
+public:
+    Libro(string t, string a) {
+        titulo = t;
+        autor = a;
+        prestado = false;
+    }
+
+    bool estaPrestado() {
+        return prestado;
+    }
+
+    void prestar() {
+        if (!prestado) {
+            prestado = true;
+            cout << "Libro prestado: " << titulo << endl;
+        } else {
+            cout << "El libro ya esta prestado.\n";
+        }
+    }
+
+    void devolver() {
+        prestado = false;
+        cout << "Libro devuelto: " << titulo << endl;
+    }
+};
+
+class Biblioteca {
+public:
+    void prestarLibro(Libro &libro) {
+        libro.prestar();
+    }
+};
+
+int main() {
+    Libro libro1("C++", "Stroustrup");
+    Biblioteca b;
+
+    b.prestarLibro(libro1);
+    b.prestarLibro(libro1); // prueba si ya esta prestado
+    libro1.devolver();
+
+    return 0;
+}
