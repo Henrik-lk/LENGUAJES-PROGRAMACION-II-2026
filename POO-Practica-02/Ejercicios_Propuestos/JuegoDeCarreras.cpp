1d1c37155af3ebe30c5b55cbc9bdc2cb7e13ae0b
Henrik-lk <micky.llave@estudiante.ucsm.edu.pe>
2026-03-21 22:20:58 -0500

    Update JuegoDeCarreras.cpp

--- POO-Practica-02/Ejercicios_Propuestos/JuegoDeCarreras.cpp
+++ POO-Practica-02/Ejercicios_Propuestos/JuegoDeCarreras.cpp
@@ -1,56 +1,67 @@
 #include <iostream>
+#include <string>
 using namespace std;
- 
+
 class Auto {
 private:
     string nombre;
-    double posicion;
-    double velocidadMax;
+    int velocidad;
+
 public:
-    Auto(string n, double vm) : nombre(n), posicion(0), velocidadMax(vm) {}
-    string getNombre()   const { return nombre;   }
-    double getPosicion() const { return posicion; }
- 
-    void avanzar(double distancia) {
-        double avance = (rand() % (int)velocidadMax) + 10;
-        posicion += avance;
-        cout << nombre << " avanza " << avance << " m -> Pos: " << posicion << " m" << endl;
+    Auto(string n, int v) {
+        nombre = n;
+        velocidad = v;
+    }
+
+    int avanzar() {
+        return velocidad;
+    }
+
+    string getNombre() {
+        return nombre;
     }
 };
- 
+
 class Carrera {
 private:
-    double distanciaTotal;
-    int    dificultad;   // 1=facil 2=medio 3=dificil
+    int distanciaTotal;
+    Auto jugador;
+    Auto computadora;
+
 public:
-    Carrera(double dist, int dif) : distanciaTotal(dist), dificultad(dif) {}
- 
-    void simular(Auto& jugador, Auto& cpu) {
-        srand((unsigned)time(nullptr));
-        cout << "=== CARRERA INICIADA (" << distanciaTotal << " m) ===" << endl;
-        int vuelta = 1;
-        while (jugador.getPosicion() < distanciaTotal &&
-               cpu.getPosicion()    < distanciaTotal) {
-            cout << "-- Vuelta " << vuelta++ << " --" << endl;
-            jugador.avanzar(distanciaTotal);
-            cpu.avanzar(distanciaTotal * dificultad * 0.1);
-        }
-        determinarGanador(jugador, cpu);
+    Carrera(int distancia, Auto j, Auto c) : jugador(j), computadora(c) {
+        distanciaTotal = distancia;
     }
- 
-    void determinarGanador(const Auto& a, const Auto& b) const {
-        cout << "=== RESULTADO ==" << endl;
-        if (a.getPosicion() >= distanciaTotal)
-            cout << a.getNombre() << " gana la carrera!" << endl;
-        else
-            cout << b.getNombre() << " (CPU) gana la carrera!" << endl;
+
+    void iniciarCarrera() {
+        int avanceJugador = 0;
+        int avanceCPU = 0;
+
+        while (avanceJugador < distanciaTotal && avanceCPU < distanciaTotal) {
+            avanceJugador += jugador.avanzar();
+            avanceCPU += computadora.avanzar();
+
+            cout << jugador.getNombre() << " lleva: " << avanceJugador << " metros" << endl;
+            cout << computadora.getNombre() << " lleva: " << avanceCPU << " metros" << endl;
+            cout << "-------------------------" << endl;
+        }
+
+        if (avanceJugador >= distanciaTotal && avanceCPU >= distanciaTotal) {
+            cout << "Empate." << endl;
+        } else if (avanceJugador >= distanciaTotal) {
+            cout << "Ganó " << jugador.getNombre() << endl;
+        } else {
+            cout << "Ganó " << computadora.getNombre() << endl;
+        }
     }
 };
- 
+
 int main() {
-    Auto jugador("Jugador", 80.0);
-    Auto cpu("CPU",     70.0);
-    Carrera carrera(500.0, 2);
-    carrera.simular(jugador, cpu);
+    Auto jugador("Auto Rojo", 20);
+    Auto cpu("Auto Azul", 18);
+
+    Carrera carrera(100, jugador, cpu);
+    carrera.iniciarCarrera();
+
     return 0;
 }
