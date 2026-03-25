5bdc6f9a44a5dc3a0684f2108dd69ec06218ec90
Puchi1402 <andremy1402@gmail.com>
2026-03-25 18:29:04 -0500

    Implement Liskov Substitution Principle with task classes

--- POO-Practica-03/Actividades/LSP.cpp
+++ POO-Practica-03/Actividades/LSP.cpp
@@ -1,1 +1,38 @@
+//Subclases intercambiables
+class Tarea {
+public:
+    virtual void mostrarDetalle() {
+        cout << "Tarea general" << endl;
+    }
+    virtual ~Tarea() {}
+};
 
+class TareaSimple : public Tarea {
+public:
+    void mostrarDetalle() override {
+        cout << "Tarea simple" << endl;
+    }
+};
+
+class TareaUrgente : public Tarea {
+public:
+    void mostrarDetalle() override {
+        cout << "Tarea urgente" << endl;
+    }
+};
+//No añadir comportamientos incorrectos
+class TareaInvalida : public Tarea {
+public:
+    void eliminarSistema() { } 
+};
+//Cumplimiento de contratos
+int main() {
+    Tarea* t1 = new TareaSimple();
+    Tarea* t2 = new TareaUrgente();
+
+    t1->mostrarDetalle();
+    t2->mostrarDetalle();
+
+    delete t1;
+    delete t2;
+}
