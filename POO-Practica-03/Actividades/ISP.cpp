367e530dd33339a2e7b0efc276bb220b5cf04402
Henrik-lk <micky.llave@estudiante.ucsm.edu.pe>
2026-03-28 21:10:18 -0500

    Refactor task management classes and interfaces

--- POO-Practica-03/Actividades/ISP.cpp
+++ POO-Practica-03/Actividades/ISP.cpp
@@ -1,33 +1,69 @@
-//Interfaces específicas
-class IAsignacion {
+#include <iostream>
+#include <string>
+using namespace std;
+
+class ICreacionTarea {
 public:
-    virtual void asignarUsuario(string usuario) = 0;
+    virtual void crearTarea(const string& titulo) = 0;
+    virtual ~ICreacionTarea() = default;
 };
 
-class IProgreso {
+class IAsignacionTarea {
 public:
-    virtual void actualizarProgreso(int porcentaje) = 0;
+    virtual void asignarTarea(const string& titulo, const string& usuario) = 0;
+    virtual ~IAsignacionTarea() = default;
 };
 
-class IReporte {
+class ISeguimientoTarea {
 public:
-    virtual void generarReporte() = 0;
+    virtual void actualizarEstado(const string& titulo, const string& estado) = 0;
+    virtual ~ISeguimientoTarea() = default;
 };
-//Evitar métodos innecesarios
-class TareaUsuario : public IAsignacion, public IProgreso {
+
+class CreadorTareas : public ICreacionTarea {
 public:
-    void asignarUsuario(string usuario) override {
-        cout << "Asignado a: " << usuario << endl;
+    void crearTarea(const string& titulo) override {
+        cout << "Se creo la tarea: " << titulo << '\n';
+    }
+};
+
+class AsignadorTareas : public IAsignacionTarea {
+public:
+    void asignarTarea(const string& titulo, const string& usuario) override {
+        cout << "Se asigno la tarea '" << titulo << "' al usuario " << usuario << '\n';
+    }
+};
+
+class MonitorTareas : public ISeguimientoTarea {
+public:
+    void actualizarEstado(const string& titulo, const string& estado) override {
+        cout << "La tarea '" << titulo << "' ahora esta en estado: " << estado << '\n';
+    }
+};
+
+class CoordinadorProyecto : public ICreacionTarea, public IAsignacionTarea {
+public:
+    void crearTarea(const string& titulo) override {
+        cout << "[Coordinador] Nueva tarea creada: " << titulo << '\n';
     }
 
-    void actualizarProgreso(int porcentaje) override {
-        cout << "Progreso: " << porcentaje << "%" << endl;
+    void asignarTarea(const string& titulo, const string& usuario) override {
+        cout << "[Coordinador] Tarea '" << titulo << "' asignada a " << usuario << '\n';
     }
 };
-//Interfaces coherentes
-class ReporteTareas : public IReporte {
-public:
-    void generarReporte() override {
-        cout << "Generando reporte..." << endl;
-    }
-};
+
+int main() {
+    CreadorTareas creador;
+    AsignadorTareas asignador;
+    MonitorTareas monitor;
+    CoordinadorProyecto coordinador;
+
+    creador.crearTarea("Diseñar base de datos");
+    asignador.asignarTarea("Diseñar base de datos", "Carlos");
+    monitor.actualizarEstado("Diseñar base de datos", "En proceso");
+
+    coordinador.crearTarea("Documentar API");
+    coordinador.asignarTarea("Documentar API", "Lucia");
+
+    return 0;
+}
